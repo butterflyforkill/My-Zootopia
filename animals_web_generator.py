4b8ae79e2b41bee2113075037922fdb35be0abcc
butterflyforkill <vasse.paul@gmail.com>
2024-06-12 22:06:16 +0200

    Add formatting for the animal info: li ul

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -37,14 +37,20 @@ def animal_info(animal):
         output += "<div class='card__title'>"
         output += f"{animal['name']}<br/>\n"
         output += '</div>'
+    output += '<p class="card__text"> <ul>'
     if "diet" in characteristics:
-        output += '<p class="card__text">'
-        output += f"<strong>Diet:</strong> {characteristics['diet']}<br/>\n"
+        output += '<li>'
+        output += f"<strong>Diet:</strong> {characteristics['diet']}\n"
+        output += '</li>'
     if "locations" in animal and animal["locations"]:  # Check for non-empty list
-        output += f"<strong>Location:</strong> {animal['locations'][0]}<br/>\n"
+        output += '<li>'
+        output += f"<strong>Location:</strong> {animal['locations'][0]}\n"
+        output += '</li>'
     if "type" in characteristics:
-        output += f"<strong>Type:</strong> {characteristics['type']}<br/>\n" 
-        output += '</p>'
+        output += '<li>'
+        output += f"<strong>Type:</strong> {characteristics['type']}\n" 
+        output += '</li>'
+    output += '</ul> </p>'    
     return output
 
 
@@ -78,6 +84,26 @@ def write_new_html(data, html_name):
 
 
 def main():
+    """
+    Generates an HTML file containing information about animals.
+
+    This function reads animal data from a JSON file, retrieves an HTML template,
+    formats the animal information, and replaces a placeholder in the template
+    with the formatted information. Finally, it writes the modified HTML content
+    to a new HTML file.
+
+    **Assumptions:**
+
+        - The `load_data` function can load JSON data from a file.
+        - The `read_html` function can read HTML content from a file.
+        - The `all_animal_info` function takes animal data and returns formatted information.
+        - The `write_new_html` function can write HTML content to a file.
+
+    **Note:**
+
+        - This function assumes the HTML template has a placeholder named "__REPLACE_ANIMALS_INFO__".
+
+    """
     animals_data = load_data('animals_data.json')
     html_string = read_html('animals_template.html')
     animal_info = all_animal_info(animals_data)
